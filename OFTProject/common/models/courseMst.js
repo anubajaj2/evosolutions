ada414f1a392f69b246b01bae3b6816a5ba2d85b
TharejaPooja <tharejapooja@gmail.com>
2019-08-22 11:53:44 +0530

    Fixed issue For Coursemst

--- OFTProject/common/models/courseMst.js
+++ OFTProject/common/models/courseMst.js
@@ -1,6 +1,6 @@
 'use strict';
 
-module.exports = function(Course) {
+module.exports = function(CourseMst) {
     var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     //validations
     CourseMst.validatesPresenceOf('CourseName', {message: 'Course Name Cannot be blank'});
