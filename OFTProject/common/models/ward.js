0221caf0238113a3f06bc71591450e99e525f136
soyuztechnologies <contact@soyuztechnologies.com>
2023-04-21 11:00:12 +0530

    ward exit handler ward.js file for roll number

--- OFTProject/common/models/ward.js
+++ OFTProject/common/models/ward.js
@@ -1,10 +1,17 @@
 'use strict';
 
-module.exports = function(Inquiry) {
+module.exports = function(Ward) {
 
     var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     var app = require('../../server/server');
 
+    Ward.observe("before save", function (ctx,next) {
+      console.log(ctx.instance);
+      //Ward.count();
+      //count++
+      //ctx.instance.rollNo = counter
+      next();
+    });
     // Inquiry.observe("before save",function(ctx, next){
     //   var app = require('../../server/server');
     //   var AppUser = app.models.AppUser;
