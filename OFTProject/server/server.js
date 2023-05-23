758597fdef5f1894858e2dc02f4ffff24a3bebac
nishan-nainsukha <nishannainsukha@soyuztechnologies.com>
2023-05-23 18:49:02 +0530

    excel date formate

--- OFTProject/server/server.js
+++ OFTProject/server/server.js
@@ -1308,7 +1308,9 @@ app.start = function () {
 									"ParentMailId":  oParent[0].__data.EmailId,
 									"ContactNo":  oParent[0].__data.Phone,
 									"Course": oCurse[0].__data.BatchNo,
-									"RegistrationDate": oSubs[0].__data.CreatedOn
+									"RegistrationDate": oSubs[0].__data.CreatedOn?oSubs[0].__data.CreatedOn.toLocaleString('en-GB', {
+										hour12: false,
+									  }):oSubs[0].__data.CreatedOn
 								});
 	
 							}
